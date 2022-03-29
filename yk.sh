6cca0f08dc6e6b00a26e4b1f739fc4cb9d7f56e4
kongmadai <51848835+kongmadai@users.noreply.github.com>
2022-03-29 16:35:12 +0800

    Update yk.sh

--- yk.sh
+++ yk.sh
@@ -6,3 +6,10 @@
 #=================================================
 echo 'modify completed.'
 
+
+# Add luci-app-ssr-plus
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+popd
+

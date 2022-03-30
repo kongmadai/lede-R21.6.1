4fe5073e24fdca61c3c69b6502cb77694f6b26e0
kongmadai <51848835+kongmadai@users.noreply.github.com>
2022-03-30 18:38:07 +0800

    Update yk.sh

--- yk.sh
+++ yk.sh
@@ -13,3 +13,8 @@ git clone --depth=1 https://github.com/fw876/helloworld
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 popd
 
+#添加smartdns
+#git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
+git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+

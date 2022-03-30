46f1670c4045c47362c7a9aa55c6dc81f737e494
kongmadai <51848835+kongmadai@users.noreply.github.com>
2022-03-30 18:51:24 +0800

    Update yk.sh

--- yk.sh
+++ yk.sh
@@ -8,6 +8,9 @@ echo 'modify completed.'
 
 
 # Add luci-app-ssr-plus
+rm -rf package/helloworld
+rm -rf package/lean/luci-app-ssr-plus
+rm -rf package/lean/xray-core
 pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
